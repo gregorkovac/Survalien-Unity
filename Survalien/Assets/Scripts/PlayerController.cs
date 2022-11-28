728e92199282b3928cd7f4e8fc0a3fee7414f425
Gregor Kovač <79193306+gregorkovac@users.noreply.github.com>
2022-11-28 09:33:42 +0100

    Camera tilt

--- Survalien/Assets/Scripts/PlayerController.cs
+++ Survalien/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
-    public GameObject playerModel;
+    public Transform playerTransform;
+    public Transform cameraTransform;
     private Rigidbody rb;
     private Vector3 movement;
+    
+    private float cameraRotationOffsetX;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        cameraRotationOffsetX = cameraTransform.rotation.eulerAngles.x;
     }
 
     // Update is called once per frame
@@ -21,13 +25,17 @@ public class PlayerController : MonoBehaviour
         movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
         Vector3 mousePos = Input.mousePosition;
+
+        cameraTransform.rotation = Quaternion.Euler(cameraRotationOffsetX - mousePos.y / 100 + 5, mousePos.x / 100 - 5, cameraTransform.rotation.z);
+
         mousePos.z = 0;
-        Vector3 playerPos = Camera.main.WorldToScreenPoint(playerModel.transform.position);
+        Vector3 playerPos = Camera.main.WorldToScreenPoint(playerTransform.position);
         mousePos.x = mousePos.x - playerPos.x;
         mousePos.y = mousePos.y - playerPos.y;
 
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        playerModel.transform.rotation = Quaternion.Euler(new Vector3(0, -angle - 270, 0));
+
+        playerTransform.rotation = Quaternion.Euler(new Vector3(0, -angle - 270, 0));
     }
 
     void FixedUpdate()
