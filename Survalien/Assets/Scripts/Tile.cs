0275d1664e1ce8d14caefa7211abf8ed81b51202
spin311 <svit.spindler@gmail.com>
2023-01-05 14:01:03 +0100

    Merge branch 'space' into HEAD

---
+++ Survalien/Assets/Scripts/Tile.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Vector2[] enemieLocations;
+    public Vector2[] spacePartLocations;
+}
