5a6144c1ef5cfe94cca925b448bf39fe92267705
spin311 <svit.spindler@gmail.com>
2023-01-06 22:32:53 +0100

    drei vier, offizier

---
+++ Survalien/Assets/Scripts/Grenade.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    private Transform playerTransform;
+    private float timer;
+    private float half;
+    private GameObject owner;
+    [SerializeField] GameObject explosion;
+    private Vector3 playerPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 1.5f;
+        playerTransform = GameObject.Find("Player").transform;
+        playerPosition =playerTransform.position;
+        half = (transform.position.x + playerPosition.x )/ 2 - 0.33f;
+
+        
+    }
+
+    void Update(){
+        timer -= Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, playerPosition,0.03f);
+            if(transform.position.x > half){
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.03f, transform.position.z);
+            }
+            else if( transform.position.x < half && transform.position.y > 0.1f) {
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.03f, transform.position.z);
+            }
+
+
+        if(timer <= 0){
+            Explode();
+        }
+    }
+
+        public void SetOwner(GameObject owner)
+    {
+        this.owner = owner;
+    }
+
+
+    void Explode(){
+        GameObject newExplosion = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(newExplosion, 1.4f);
+          if (Vector3.Distance(this.transform.position, playerTransform.position) < 3.33f) {
+             playerTransform.gameObject.GetComponent<CharacterController>().DecreaseHealth(2);
+
+          }
+           Soldier[] soldiers = FindObjectsOfType<Soldier>();
+                    foreach (Soldier soldier in soldiers) {
+                        if (Vector3.Distance(this.transform.position, soldier.transform.position) < 3.33f) {
+                            soldier.transform.gameObject.GetComponent<CharacterController>().DecreaseHealth(5);
+                        }
+                }
+            Enemy[] enemies = FindObjectsOfType<Enemy>();
+                    foreach (Enemy enemy in enemies) {
+                        if (Vector3.Distance(this.transform.position, enemy.transform.position) < 3.33f) {
+                            enemy.transform.gameObject.GetComponent<CharacterController>().DecreaseHealth(5);
+                        }
+                }
+
+
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ Survalien/Assets/Scripts/Soldier.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour
+{
+    enum State
+    {
+        Idle,
+        Searching,
+        Attacking,
+        Stalking
+    }
+
+    public GameObject grenadePrefab;
+    public GameObject projectilePrefab;
+    public GameObject pistolFireProjectile;
+    public float timeBetweenStateChanges = 5.0f;
+
+    public Transform bulletSpawnPoint;
+
+    private CharacterController characterController; 
+    private Transform playerTransform;  
+    private Vector3 lastKnownPlayerPosition;
+
+    private Animator animator;
+
+    private bool isDead;
+    private bool startThrowing;
+
+    [SerializeField] private State state;
+    [SerializeField] private float stateChangeTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = this.GetComponent<CharacterController>();
+        animator = this.transform.GetChild(0).GetComponent<Animator>();
+
+        playerTransform = GameObject.Find("Player").transform;
+
+        state = State.Idle;
+        stateChangeTimer = timeBetweenStateChanges;
+
+        isDead = false;
+        startThrowing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if(!startThrowing && characterController.health < 6) {
+                InvokeRepeating("ThrowGrenade", 1, 1.5f);
+                CancelInvoke("Shoot");
+                startThrowing = true;
+                }
+
+        if (isDead){
+            return;
+        }
+        else if (state != State.Attacking) {
+            // If the player is in sight, start attacking
+            if (Vector3.Distance(transform.position, playerTransform.position) < 10f && 
+                characterController.isVisionUnobstructed(this.gameObject, playerTransform.gameObject))
+            {
+                characterController.Idle();
+                if(!startThrowing) {
+                InvokeRepeating("Shoot", 1, 0.7f);
+                }
+
+                animator.SetBool("IsShooting", true);
+                animator.SetBool("IsRunning", false);
+                animator.SetBool("IsWalking", false);
+                state = State.Attacking;
+            } else {
+                // Change state from idle to searching and vice versa
+                stateChangeTimer -= Time.deltaTime;
+                if (stateChangeTimer <= 0) {
+                    stateChangeTimer = timeBetweenStateChanges;
+                    if (state == State.Idle) {
+                        characterController.RotateRandom();
+                        state = State.Searching;
+                        animator.SetBool("IsWalking", true);
+                    } else if (state == State.Searching) {
+                        state = State.Idle;
+                        animator.SetBool("IsWalking", false);
+                    }
+                }
+            }
+        }
+
+        switch (state) {
+            case State.Idle:
+                characterController.Idle();
+            break;
+
+            case State.Searching:
+                characterController.Move();
+            break;
+
+            case State.Attacking:
+                if (playerTransform.GetComponent<CharacterController>().health <= 0) {
+                    CancelInvoke("Shoot");
+                    CancelInvoke("ThrowGrenade");
+                    state = State.Idle;
+                    animator.SetBool("IsShooting", false);
+                    animator.SetBool("IsRunning", false);
+                    animator.SetBool("IsWalking", false);
+                }
+                else if (Vector3.Distance(transform.position, playerTransform.position) > 10f || 
+                    !characterController.isVisionUnobstructed(this.gameObject, playerTransform.gameObject))
+                {
+                    CancelInvoke("Shoot");
+                    state = State.Stalking;
+                    animator.SetBool("IsRunning", true);
+                    animator.SetBool("IsShooting", false);
+                    characterController.Sprinting();
+                } else {
+                    characterController.RotateTowards(playerTransform.position);
+                    lastKnownPlayerPosition = playerTransform.position;
+                }
+            break;
+
+            case State.Stalking:
+                // Move to the last known player position
+                characterController.Move();
+                characterController.RotateTowards(lastKnownPlayerPosition);
+                if (Vector3.Distance(transform.position, lastKnownPlayerPosition) < 1f)
+                {
+                    characterController.Walking();
+                    state = State.Idle;
+                    animator.SetBool("IsRunning", false);
+                    animator.SetBool("IsWalking", false);
+                }
+            break;
+        }
+    }
+    
+    void Shoot() {
+        animator.SetTrigger("Shoot");
+        GameObject projectile = Instantiate(projectilePrefab, bulletSpawnPoint.position, this.transform.rotation);
+        GameObject pistolFire = Instantiate(pistolFireProjectile, bulletSpawnPoint.position, this.transform.rotation);
+
+        projectile.transform.position += projectile.transform.forward * 0.5f;
+        pistolFire.transform.position += pistolFire.transform.forward * 0.5f;
+
+        projectile.GetComponent<ProjectileController>().SetOwner(this.gameObject);
+    }
+    void ThrowGrenade(){
+        animator.SetTrigger("Shoot");    
+        GameObject grenade = Instantiate(grenadePrefab, bulletSpawnPoint.position, this.transform.rotation);
+        }
+
+    public void OnDeath() {
+        CancelInvoke();
+        characterController.Idle();
+        animator.SetBool("IsRunning", false);
+        animator.SetBool("IsWalking", false);
+        animator.SetBool("IsShooting", false);
+        animator.SetTrigger("Death");
+        isDead = true;
+
+        StartCoroutine(DeathAnimation());
+
+        //this.GetComponent<CapsuleCollider>().height = 1f;
+    }
+
+    IEnumerator DeathAnimation() {
+        yield return new WaitForSeconds(2f);
+        //Destroy(this.gameObject);
+        this.GetComponent<CapsuleCollider>().height = 1.5f;
+    }
+
+    // Rotate towards player if you get alerted
+    public void Alert() {
+        if (state == State.Idle || state == State.Searching || state == State.Stalking) {
+            state = State.Stalking;
+            animator.SetBool("IsWalking", true);
+            lastKnownPlayerPosition = playerTransform.position;
+        }
+    }
+}
